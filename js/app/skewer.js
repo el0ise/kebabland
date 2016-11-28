748d9f1ba892cae3d374042cffd21189ea771f24
pradyutbansal <pbansal@macalester.edu>
2016-11-28 16:36:14 -0600

    Merge branch 'master' of https://github.com/el0ise/kebabland
    # Conflicts:
    #	index.html

--- js/app/skewer.js
+++ js/app/skewer.js
@@ -8,5 +8,30 @@ define(function(){
 
 	}
 
-	Skewer.prototype
+	Skewer.prototype = {
+		setPos: function(x, y) {
+			this.x = x;
+			this.y = y;
+		},
+
+		setDirection: function(dx, dy) {
+			this.dx = dx;
+			this.dy = dy;
+		},
+
+		move: function(court) {
+			this.x += this.dx;
+			this.y += this.dy;
+		},
+
+		draw: function(canvasContext) {
+			canvasContext.beginPath();
+			canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+			canvasContext.closePath();
+			canvasContext.fillStyle = "black";
+			canvasContext.fill();
+		}
+	}
+
+	return Skewer;
 })
