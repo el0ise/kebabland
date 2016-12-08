f97fefd394411ef7cd32cb7f03959f9162ea06d9
el0ise <eterry@macalester.edu>
2016-12-07 18:12:06 -0600

    idk

--- js/app/ingredients.js
+++ js/app/ingredients.js
@@ -1,8 +1,8 @@
 define(["app/gameMath"], function(gameMath)){
 
-    function Ingredient(x, y, type){
-        this.x = x;
-        this.y = y;
+    function Ingredient(type){
+        this.x = 0//????x;
+        this.y = 0//?????y;
         this.type = type;
         this.inSnake = false;
 
@@ -10,6 +10,26 @@ define(["app/gameMath"], function(gameMath)){
 
     Ingredient.prototype = {
         testHit : function(){
+            // Snake is moving up
+            if (the_big_kebab.direction == "up"){
+
+            }
+
+            // Snake is moving down
+            if (the_big_kebab.direction == "down"){
+
+            }
+
+            // Snake is moving left
+            if (the_big_kebab.direction == "left"){
+
+            }
+
+            // Snake is moving right
+            if (the_big_kebab.direction == "right"){
+
+            }
+
 
         }
         draw: function(canvasContext){
